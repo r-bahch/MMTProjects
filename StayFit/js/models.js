b4db2b60e12d922a52e650d1ddf18a00b3d74a2e
Radoslav Bahchevanov <r.bahchevanov@gmail.com>
2016-06-19 10:19:27 +0300

    Branch test

---
+++ StayFit/js/models.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="knockout-3.4.0.js" />
+
+var Exercise = function () {
+    var self = this;
+    self.id = 0;
+    self.name = "";
+    self.description = "";
+    self.muscleGroup = null;
+    self.picUrl = "";
+    self.videoUrl = "";
+}
---
+++ StayFit/js/site.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="knockout-3.4.0.js" />
+/// <reference path="models.js" />
+
+var appViewModel = function () {
+    this.exercises = new Developer("Radoslav", "Bahchevanov");
+}
+
+
+ko.applyBindings(new appViewModel);
